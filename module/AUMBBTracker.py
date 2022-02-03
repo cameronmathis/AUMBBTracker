1847df53f3998ee1d3a7f4c12448adb0a44aeb6d
Cameron Mathis <cmmathis98@gmail.com>
2022-02-03 15:51:38 -0500

    reworks utils

--- module/AUMBBTracker.py
+++ module/AUMBBTracker.py
@@ -1,5 +1,46 @@
-from Utils.Util import getDaysSinceLoss
-from Utils.TwitterUtil import sendTweet
+from datetime import date, datetime
+from utils.TwitterUtil import sendTweet
+from utils.ScraperUtil import *
+from utils.DatabaseUtil import *
+
+'''
+Checks to see if a loss has occurred
+
+Returns a boolean
+'''
+
+
+def haveLost():
+    # get previous losses
+    previousRecord = getRecord()
+    previousLosses = previousRecord.split("-", 1)[1]
+    # get current losses
+    currentRecord = scrapeCurrentRecord()
+    currentLosses = currentRecord.split("-", 1)[1]
+    # compare losses
+    if not (previousLosses == currentLosses):
+        return True
+
+    return False
+
+
+'''
+Calculates the number of days since a loss
+
+Returns a int
+'''
+
+
+def getDaysSinceLoss():
+    if (haveLost()):
+        setLastLossDate(scrapeLastLossDate())
+
+    today = datetime.strptime(date.today().strftime("%m/%d/%Y"), "%m/%d/%Y")
+    lastLostDate = datetime.strptime(getLastLossDate(), "%m/%d/%Y")
+    daysSinceLoss = today - lastLostDate
+
+    return daysSinceLoss.days
+
 
 '''
 Main method to run the application
