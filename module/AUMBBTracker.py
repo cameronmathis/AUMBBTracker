1c4de028664e041e6c2b6d67a749576b9112dc1a
Cameron Mathis <cmmathis98@gmail.com>
2022-02-03 16:59:44 -0500

    ready for testing

--- module/AUMBBTracker.py
+++ module/AUMBBTracker.py
@@ -48,9 +48,8 @@ Main method to run the application
 
 
 def main():
-    print(f"It has been {getDaysSinceLoss()} days since AU MBB lost.")
-    # sendTweet(
-    #     f"It has been {getDaysSinceLoss()} days since AU MBB lost.")
+    sendTweet(
+        f"It has been {getDaysSinceLoss()} days since AU MBB lost.")
 
 
 # call the main method
